59b435d7dc957b1204510b50216956c428c9e62a
aLe3ouLa <barka@csd.uoc.gr>
2019-05-01 13:33:27 +0300

    Add weather widget

--- js/app.js
+++ js/app.js
@@ -14,7 +14,7 @@ var getCurrentLocation = function() {
       showPosition, // Success callback function
       showError, // Error callback function
       positionOptions // Position options
-    );  
+    );
   } else {
     // Geolocation not supported
     console.log("Geolocation is not supported in your browser");
@@ -29,14 +29,16 @@ function showPosition(position) {
       "Longitude: " +
       position.coords.longitude
   );
-  initMap(position.coords.latitude, position.coords.longitude);
-  document.getElementById("weather-container--Latitude").innerHTML = position.coords.latitude;
-  document.getElementById("weather-container--Longitude").innerHTML = position.coords.longitude;
-
+  fn_getWeatherByLL(position.coords.latitude, position.coords.longitude);
+//   initMap(position.coords.latitude, position.coords.longitude);
+  document.getElementById("weather-container--Latitude").innerHTML =
+    position.coords.latitude;
+  document.getElementById("weather-container--Longitude").innerHTML =
+    position.coords.longitude;
 }
 
 function showError(error) {
-    // Error callback function
+  // Error callback function
   switch (error.code) {
     case error.PERMISSION_DENIED:
       console.log("User denied the request for Geolocation.");
@@ -54,34 +56,76 @@ function showError(error) {
 }
 
 function getCurrentDate() {
-    console.log(new Date())
-    document.getElementById('weather-current-date').innerHTML = formatDate(new Date());
+  console.log(new Date());
+  document.getElementById("weather-current-date").innerHTML = formatDate(
+    new Date()
+  );
 }
 
 function formatDate(date) {
-    var monthNames = [
-      "January", "February", "March",
-      "April", "May", "June", "July",
-      "August", "September", "October",
-      "November", "December"
-    ];
-  
-    var day = date.getDate();
-    var monthIndex = date.getMonth();
-    var year = date.getFullYear();
-  
-    return day + ' ' + monthNames[monthIndex] + ' ' + year;
-  }
-  
+  var monthNames = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December"
+  ];
+
+  var day = date.getDate();
+  var monthIndex = date.getMonth();
+  var year = date.getFullYear();
+
+  return day + " " + monthNames[monthIndex] + " " + year;
+}
+
 /** Initialize map from google API */
-  function initMap(lat, lng) {
-    // The location of user
-    var currentPos = { lat: lat, lng: lng };
-    // The map, centered at Athens
-    var map = new google.maps.Map(document.getElementById("googleMap"), {
-      zoom: 15,
-      center: currentPos
-    });
-    // The marker, positioned at Athens
-    var marker = new google.maps.Marker({ position: currentPos, map: map });
-  }
+function initMap(lat, lng) {
+  // The location of user
+  var currentPos = { lat: lat, lng: lng };
+  // The map, centered at Athens
+  var map = new google.maps.Map(document.getElementById("googleMap"), {
+    zoom: 15,
+    center: currentPos
+  });
+  // The marker, positioned at Athens
+  var marker = new google.maps.Marker({ position: currentPos, map: map });
+}
+
+// ---------------
+// Weather API
+// ---------------
+
+_dsSecret = "466a8f2a294a5c0b2507f45e5f1298f7"; //Again, for testing only, should be hidden in proxy
+
+function fn_getWeatherByLL(geoLat,geoLng){
+  //API Variables
+  var proxy = 'https://cors-anywhere.herokuapp.com/';
+  var dsAPI = "https://api.darksky.net/forecast/";
+  var dsKey = _dsSecret + "/";
+  var dsParams = "?exclude=minutely,hourly,daily,alerts,flags&units=auto";
+  //Concatenate API Variables into a URLRequest
+  var URLRequest = proxy + dsAPI + dsKey + String(geoLat) + "," + String(geoLng) + dsParams
+  //Make the jQuery.getJSON request
+  $.getJSON( URLRequest )
+    //Success promise
+    .done(function( data ) {
+      var wSummary = data.currently.summary;
+      var wTemperature = data.currently.temperature;
+      console.log(wSummary);
+      console.log(wTemperature);
+      // lots of results available on the data object
+      // use the results to populate the GUI here
+    })
+    //Error promise
+    .fail(function() {
+      alert('Sorry, something bad happened when retrieving the weather');
+    }
+  );
+}
