e100b4ca1c0c1288054e935acd2a6f26af020426
Alexandra <barka.alexandra2@gmail.com>
2026-08-20 10:05:05 +0200

    remove hero from error

--- src/components/ErrorState.tsx
+++ src/components/ErrorState.tsx
@@ -3,7 +3,7 @@ import './ErrorState.css';
 export const ErrorState = ({ refetch }: { refetch: () => void}) => {
     return (
         <section className="error-state-container">
-            <img width="24" alt="Something went wrong" src="src/assets/images/icon-error.svg" />
+            <img width="24" alt="Something went wrong" src="src/assets/images/icon-error.svg" aria-hidden='true' />
             <h1>Something went wrong</h1>
             <p className="error-state-description">We couldn't connect to the server (API error). Please try again in a few moments.</p>
             <button className="retry-button" onClick={refetch}>
